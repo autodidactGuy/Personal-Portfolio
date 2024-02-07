13882e48e5ae293fbf2f9756d765c3c99c50a549
Hassan Raza <hassanraza632@gmail.com>
2024-02-07 15:41:06 -0600

    Contact form validation and small fixes

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,22 +1,152 @@
+import React from "react";
+import { z } from "zod";
+import { useZodForm } from "@/hooks/useZodForm";
 import DefaultLayout from "@/layouts/default";
+import { Toaster, toast } from 'sonner';
 import { siteConfig } from "@/config/site";
 import { InlineWidget} from "react-calendly";
-import { Accordion, AccordionItem, Button, Input, Kbd, Textarea } from "@nextui-org/react";
+import { Accordion, AccordionItem, Button, Input, Kbd, Spinner, Textarea } from "@nextui-org/react";
+import { Controller, SubmitHandler } from "react-hook-form";
+import { useTheme } from "next-themes";
 
+
+interface ContactData {
+    name: string;
+    email: string;
+    phone: string;
+    subject: string;
+    message: string;
+}
+
+const contactFormSchema = z.object({
+    name: z.string().min(1, { message: "Name is required" }),
+    email: z.string().email({ message: "Invalid email address" }),
+    phone: z.string().min(10, "Invalid Phone Number").max(10, "Invalid Phone Number"),
+    subject: z.string().min(10, "Subject should be more than 10 characters atleast."),
+    message: z.string().min(10, "Message should be more than 10 characters atleast.")
+  });
 
 export default function Contact() {
 
+    const { theme } = useTheme();
+    const toastTheme = theme as "light" | "dark" | "system";
+
+    const {
+        control,
+        handleSubmit,
+        reset,
+        formState: { errors, isSubmitting },
+    } = useZodForm({
+        schema: contactFormSchema,
+        defaultValues: {
+            name: "",
+            email: "",
+            phone: "",
+            subject: "",
+            message: ""
+        },
+    });
+
+    const onSubmit: SubmitHandler<z.infer<typeof contactFormSchema>> = async (data) => {
+        try {
+            console.log(data);
+            toast.success("Contact message successfully sent!");
+            reset();
+        } catch (error) {
+            toast.error("Contact message failed to sent!")
+        }
+      };
+
     const contactForm = (
-        <form className="flex flex-col gap-3 places-items-center w-full mb-2">
+        <form
+            onSubmit={handleSubmit(onSubmit)}
+            className="flex flex-col gap-3 places-items-center w-full mb-2"
+            noValidate>
+
             <p className="text-2xl hidden lg:block font-bold pl-1 mb-2">Contact Me</p>
-            <Input type="text" variant="faded" label="Name" required/>
-            <Input type="email" variant="faded" label="Email" required/>
-            <Input type="tel" variant="faded" label="Phone" required/>
-            <Input type="text" variant="faded" label="Subject" required/>
-            <Textarea variant="faded" className="col-span-12 md:col-span-6 mb-6 md:mb-0" minRows={5} label="Message" required/>
-            <Button type="submit" variant="solid" color="primary" radius="full">
-                Send Message
+
+            <Toaster richColors theme={toastTheme} position="bottom-left"/>
+
+            <Controller
+                name="name"
+                control={control}
+                render={({ field }) => (
+                <Input
+                    {...field}
+                    type="text"
+                    variant="faded"
+                    label="Name" 
+                    isInvalid={!!errors.name}
+                    errorMessage={errors.name?.message}
+                />
+                )}
+            />
+
+            <Controller
+                name="email"
+                control={control}
+                render={({ field }) => (
+                <Input
+                    {...field}
+                    type="email"
+                    variant="faded"
+                    label="Email" 
+                    isInvalid={!!errors.email}
+                    errorMessage={errors.email?.message}
+                />
+                )}
+            />
+
+            <Controller
+                name="phone"
+                control={control}
+                render={({ field }) => (
+                <Input
+                    {...field}
+                    type="tel"
+                    variant="faded"
+                    label="Phone" 
+                    isInvalid={!!errors.phone}
+                    errorMessage={errors.phone?.message}
+                />
+                )}
+            />
+
+            <Controller
+                name="subject"
+                control={control}
+                render={({ field }) => (
+                <Input
+                    {...field}
+                    type="text"
+                    variant="faded"
+                    label="Subject" 
+                    isInvalid={!!errors.subject}
+                    errorMessage={errors.subject?.message}
+                />
+                )}
+            />
+
+            <Controller
+                name="message"
+                control={control}
+                render={({ field }) => (
+                <Textarea
+                    {...field}
+                    variant="faded"
+                    className="col-span-12 md:col-span-6 mb-6 md:mb-0"
+                    minRows={5}
+                    label="Message" 
+                    isInvalid={!!errors.message}
+                    errorMessage={errors.message?.message}
+                />
+                )}
+            />
+
+            <Button type="submit" variant="solid" color={(isSubmitting) ? "default" : "primary"} radius="full" disabled={isSubmitting}>
+                {isSubmitting ? <Spinner /> : 'Send Message'}
             </Button>
+
         </form>
     );
 
