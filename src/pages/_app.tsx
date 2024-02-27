25939a7ab0ea807da5830bc9fea135e71821392f
Hassan Raza <hassanraza632@gmail.com>
2024-02-27 09:34:41 -0600

    Change default theme to dark

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,7 +11,7 @@ export default function App({ Component, pageProps }: AppProps) {
 
 	return (
 		<NextUIProvider navigate={router.push}>
-			<NextThemesProvider>
+			<NextThemesProvider defaultTheme="dark">
 				<Component {...pageProps} />
 			</NextThemesProvider>
 		</NextUIProvider>
